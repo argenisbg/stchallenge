569258248da0a429f6944cf156ebe88d52e26ec2
argenisbg <argenis@servicecore.com>
2025-02-06 17:07:20 -0600

    feat(question-3): Adde questionThree php file with solution

---
+++ questionThree.php
@@ -0,0 +1,55 @@
+<?php
+
+use App\Models\Address;
+use App\Models\Cart;
+use App\Models\Customer;
+use App\Models\Item;
+
+require 'vendor/autoload.php';
+
+$itemOne = new Item();
+$itemOne->setId('0918783')
+        ->setName('Item 1')
+        ->setQuantity(1)
+        ->setPrice(10);
+
+$itemTwo = new Item();
+$itemTwo->setId('0918792')
+        ->setName('Item 2')
+        ->setQuantity(2)
+        ->setPrice(8);
+
+$addressOne = new Address();
+$addressOne->setLineOne('Rexford Dr')
+           ->setCity('Beverly Hills')
+           ->setState('CA')
+           ->setZipCode('90212');
+
+$addressTwo = new Address();
+$addressTwo->setLineOne('N Camden Dr')
+           ->setLineTwo('Apartment 10')
+           ->setCity('Beverly Hills')
+           ->setState('CA')
+           ->setZipCode('90210');
+
+$customer = new Customer();
+$customer->setFirstName('Argenis')
+         ->setLastName('Barraza')
+         ->setAddresses([$addressOne, $addressTwo]);
+
+$cart = new Cart();
+$cart->setCustomer($customer)
+     ->setShippingAddress($addressOne)
+     ->setItems([$itemOne, $itemTwo]);
+
+print_r([
+    'Customer Name'           => $cart->getCustomer()->getFullName(),
+    'Customer Addresses' => $cart->getCustomer()->getAddresses(),
+    'Cart Items'         => $cart->getItems(),
+    'Shipping Address'   => $cart->getShippingAddress(),
+    'Subtotal'           => $cart->getCartSubtotal(),
+    'Shipping'      => $cart->getCartShipping(),
+    'Tax'           => $cart->getCartSubtotalTax(),
+    'Total'              => $cart->getItemsTotalWithShippingAndTax()
+]);
+
