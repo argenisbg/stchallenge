e797ffe4e7b19e01d381f674aa250f93678a1ad9
argenisbg <argenis@servicecore.com>
2025-02-06 17:05:41 -0600

    feat(question-3): Define static function to return shipping rate

--- app/Services/ShippingService.php
+++ app/Services/ShippingService.php
@@ -4,5 +4,11 @@ namespace App\Services;
 
 class ShippingService
 {
-
+    public static function getRate(?string $zipCode) : float {
+        return match ($zipCode) {
+            '90210' => 5.30,
+            '90212' => 8.20,
+            default => 0
+        };
+    }
 }
